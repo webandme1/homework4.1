6e3013cb0c7f4796d68acb1f4ed5b257e3bf607d
webandme1 <94754776+webandme1@users.noreply.github.com>
2024-01-20 15:12:17 -0700

    init

--- demo_fileserver.js
+++ demo_fileserver.js
@@ -5,13 +5,13 @@ var fs = require('fs');
 http.createServer(function (req, res) {
   var q = url.parse(req.url, true);
   var filename = "." + q.pathname;
-  fs.readFile(products.html, function(err, data) {
+  fs.readFile(filename, function(err, data) {
     if (err) {
       res.writeHead(404, {'Content-Type': 'text/html'});
-      return res.end("Product not found");
+      return res.end("404 Not Found");
     } 
     res.writeHead(200, {'Content-Type': 'text/html'});
     res.write(data);
     return res.end();
   });
-}).listen(5500);
+}).listen(8010);
